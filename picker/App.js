48bbd2c40d9724fb651fd192c54f3088874e993f
Babouze <ivorydu13@gmail.com>
2018-03-06 16:30:33 +0100

    Added login page

--- picker/App.js
+++ picker/App.js
@@ -1,24 +1,55 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {} from 'react-materialize';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import t from 'tcomb-form-native'; // 0.6.9
+
+const Form = t.form.Form;
+
+const Login = t.struct({
+    Identifiant: t.String,
+});
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+    handleSubmit = () => {
+        const value = this._form.getValue(); // use that ref to get the form value
+        console.log('value: ', value);
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.hello}>Bonjour</Text>
+                <View style={styles.form}>
+                    <Form
+                        ref={c => this._form = c} // assign a ref
+                        type={Login}
+                    />
+                    <Button
+                        title="Se connecter"
+                        onPress={this.handleSubmit}
+                    />
+                </View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        // marginTop: 50,
+        padding: 20,
+        backgroundColor: '#ffffff',
+    },
+    form: {
+        marginTop: 100,
+    },
+    hello: {
+        fontSize: 40,
+        fontWeight: 'bold',
+        textAlignVertical: 'center',
+        textAlign: "center"
+    }
 });
