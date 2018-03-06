a839efe1af74745307f9f32c89ef6f7fd347cb39
Babouze <ivorydu13@gmail.com>
2018-03-06 18:10:03 +0100

    Added StartPicking and Picking pages. Added navigation too

--- picker/App.js
+++ picker/App.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import {AppRegistry, StyleSheet, Text, View, Button, Keyboard} from 'react-native';
+import {StackNavigator} from 'react-navigation';
+import BarcodeScanner from 'react-native-barcodescanner';
 
 import t from 'tcomb-form-native'; // 0.6.9
 
 const Form = t.form.Form;
 
-const Login = t.struct({
+const LoginStruct = t.struct({
     Identifiant: t.String,
 });
 
-export default class App extends React.Component {
+class Login extends React.Component {
+    static navigationOptions =
+        {
+            title: 'Picking',
+            headerLeft: null
+        };
+
     handleSubmit = () => {
         const value = this._form.getValue(); // use that ref to get the form value
         console.log('value: ', value);
+        if (value !== null) {
+            this.props.navigation.navigate('StartPicking');
+            Keyboard.dismiss();
+        }
     };
 
     render() {
@@ -22,7 +34,8 @@ export default class App extends React.Component {
                 <View style={styles.form}>
                     <Form
                         ref={c => this._form = c} // assign a ref
-                        type={Login}
+                        type={LoginStruct}
+                        value={{Identifiant: 'WESH'}}
                     />
                     <Button
                         title="Se connecter"
@@ -34,22 +47,119 @@ export default class App extends React.Component {
     }
 }
 
+class StartPicking extends React.Component {
+    static navigationOptions =
+        {
+            title: 'Démarrer',
+            headerLeft: null
+        };
+
+    handleSubmit = () => {
+        this.props.navigation.navigate('Picking');
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.paragraph}>Cliquez sur le bouton ci-dessous pour démarrer la préparation de
+                    commandes</Text>
+                <Button
+                    title="Démarrer"
+                    onPress={this.handleSubmit}
+                />
+            </View>
+        );
+    }
+}
+
+class Picking extends React.Component {
+    static navigationOptions =
+        {
+            title: 'Récupération d\'article',
+            headerLeft: null
+        };
+
+    handleSubmit = () => {
+        this.props.navigation.navigate('StartScan');
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.paragraph}>A122</Text>
+                <Text style={styles.paragraph}>S4</Text>
+                <Text style={styles.paragraph}>E3</Text>
+                <Button
+                    // style={styles.bottomElement}
+                    title="Scanner"
+                    onPress={this.handleSubmit}
+                />
+            </View>
+        );
+    }
+}
+
+class StartScan extends React.Component {
+    static navigationOptions =
+        {
+            title: 'Scanner l\'article',
+            headerLeft: null
+        };
+
+    handleSubmit = () => {
+        this.props.navigation.navigate('Picking');
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.paragraph}>A122</Text>
+                <Text style={styles.paragraph}>S4</Text>
+                <Text style={styles.paragraph}>E3</Text>
+                <Button
+                    // style={styles.bottomElement}
+                    title="Scanner"
+                    onPress={this.handleSubmit}
+                />
+            </View>
+        );
+    }
+}
+
+export default Project = StackNavigator(
+    {
+        Login: {screen: Login},
+        StartPicking: {screen: StartPicking},
+        Picking: {screen: Picking},
+        StartScan: {screen: StartScan}
+    });
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        // marginTop: 50,
         padding: 20,
         backgroundColor: '#ffffff',
     },
     form: {
-        marginTop: 100,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 50,
     },
     hello: {
         fontSize: 40,
         fontWeight: 'bold',
         textAlignVertical: 'center',
         textAlign: "center"
-    }
+    },
+    paragraph: {
+        paddingBottom: 30,
+        fontSize: 30,
+        textAlignVertical: 'center',
+        textAlign: "center"
+    },
+    // bottomElement: {
+    //     flexDirection: 'row',
+    //     position: 'absolute',
+    //     bottom: 0
+    // }
 });
