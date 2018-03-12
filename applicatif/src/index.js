80071bb5e9d3963625c9f376ddc1ad3871fbd00f
pierr <pierre.rudelou@hotmail.fr>
2018-03-12 17:05:18 +0100

    Applicatif : fix navbar issues

--- applicatif/src/index.js
+++ applicatif/src/index.js
@@ -319,9 +319,9 @@ function connection() {
             <div>
                 <nav>
                     <div class="nav-wrapper">
-                        <a href="#" class="brand-logo">{username}</a>
+                        <a class="brand-logo">{username}</a>
                         <ul id="nav-mobile" class="right hide-on-med-and-down">
-                            <li><a href="collapsible.html">Déconnexion</a></li>
+                            <li><a href="index.html">Déconnexion</a></li>
                         </ul>
                     </div>
                 </nav>
